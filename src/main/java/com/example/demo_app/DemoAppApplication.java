d95ffec5d76d480c844944ac409b0c7ef35fbd1f
Ayushver <vermaayu793@gmail.com>
2025-05-02 03:21:48 +0530

    kafka-practice

--- src/main/java/com/example/demo_app/DemoAppApplication.java
+++ src/main/java/com/example/demo_app/DemoAppApplication.java
@@ -1,13 +1,22 @@
 package com.example.demo_app;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoAppApplication.class, args);
+	public static void main(String[] args)  {
+	//	SpringApplication.run(DemoAppApplication.class, args);
+              ConfigurableApplicationContext ctx = SpringApplication.run(DemoAppApplication.class);
+//        RegisterUser userReg = ctx.getBean(RegisterUser.class);
+//        userReg.registerUser("mike");
+//        userReg.getUserDetails("mike");
+
+
+
 	}
 
 }
